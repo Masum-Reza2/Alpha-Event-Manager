373dd256ffb1dc90cfc633bd0b3b4752cf416f87
Masum-Reza <careerhunter4280@gmail.com>
2023-10-07 19:50:49 -0700

    enhance the beauty added spinner

--- src/MainLayOut/MainLayOut.jsx
+++ src/MainLayOut/MainLayOut.jsx
@@ -1,16 +1,25 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Navbar from "../Components/Navbar/Navbar";
 import { Toaster } from "react-hot-toast";
+import Spinner from "../Components/Spinner/Spinner";
 
 const MainLayOut = () => {
+    const navigation = useNavigation();
+    console.log(navigation)
 
     return (
         <div className="md:w-[90%] mx-auto scroll-smooth">
             <Toaster />
             <Navbar />
-            <div className="min-h-[70vh] py-2">
-                <Outlet />
-            </div>
+            {
+                navigation.state === 'loading' ?
+                    <Spinner />
+                    :
+                    <div className="min-h-[70vh] py-2">
+                        <Outlet />
+                    </div>
+            }
+
         </div>
     )
 }
--- src/PrivateRoute/PrivateRoute.jsx
+++ src/PrivateRoute/PrivateRoute.jsx
@@ -1,18 +1,14 @@
 import { Navigate, useLocation } from "react-router-dom";
 import useGlobal from "../Hooks/useGlobal";
 import PropTypes from 'prop-types';
+import Spinner from "../Components/Spinner/Spinner";
 
 const PrivateRoute = ({ children }) => {
     const { user, loading } = useGlobal();
     const { pathname } = useLocation()
 
     if (loading) {
-        return <div className="h-[80vh] flex items-center justify-center">
-            <span className="loading loading-bars loading-xs"></span>
-            <span className="loading loading-bars loading-sm"></span>
-            <span className="loading loading-bars loading-md"></span>
-            <span className="loading loading-bars loading-lg"></span>
-        </div>
+        return <Spinner />
     }
     else if (user) {
         return children
